143276f47168f3856f0585a618e286e339346949
soyeongame <81913158+soyeongame@users.noreply.github.com>
2021-04-28 17:10:04 +0900

    1

---
+++ server/constants.js
@@ -0,0 +1,35 @@
+const TILE_SIZE = 35;
+
+const EXPLOSION_TIME = 2000;
+
+const SPOIL_CHANCE = 50;
+const SPEED = 0;
+const POWER = 1;
+const DELAY = 2;
+
+const EMPTY_CELL = 0;
+const DESTRUCTIBLE_CELL = 2;
+const NON_DESTRUCTIBLE_CELL = 1;
+
+const INITIAL_POWER = 1
+const STEP_POWER = 1
+
+const SKINS = [
+  '윈디', '학계정설', '오마이갓', '덤디덤디',
+  '선장님', 'JSY엔터대표', '아자사자', '연하공주', '천진반', '힙합꾸러기', '낭만땃쥐'
+]
+
+module.exports = {
+  TILE_SIZE,
+  EXPLOSION_TIME,
+  SPOIL_CHANCE,
+  SPEED,
+  POWER,
+  DELAY,
+  EMPTY_CELL,
+  DESTRUCTIBLE_CELL,
+  NON_DESTRUCTIBLE_CELL,
+  INITIAL_POWER,
+  STEP_POWER,
+  SKINS
+}
